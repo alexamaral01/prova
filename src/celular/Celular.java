7ef43722b6e9e26d321bef4d1a4b90ccf75b1059
Alex Amaral <alexamaralsjc@gmail.com>
2023-05-24 11:45:17 -0300

    ajuste nos folder

--- src/celular/Celular.java
+++ src/celular/Celular.java
@@ -1,3 +1,4 @@
+package celular;
 public class Celular {
     
     private int ddd;
--- src/celular/CelularTest.java
+++ src/celular/CelularTest.java
@@ -1,4 +1,5 @@
-public class App {
+package celular;
+public class CelularTest {
     
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Rafael");
