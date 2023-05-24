b302cddb5f51ab335b76c80cf658fed13a68dbac
Alex Amaral <alexamaralsjc@gmail.com>
2023-05-24 11:42:48 -0300

    init

---
+++ src/Celular.java
@@ -0,0 +1,21 @@
+public class Celular {
+    
+    private int ddd;
+    private int numero;
+
+
+    public Celular(int ddd, int numero){
+        this.ddd = ddd;
+        this.numero = numero;
+    }
+
+
+    public int getDdd() {
+        return ddd;
+    }
+
+
+    public int getNumero() {
+        return numero;
+    }
+}
